73c6201565ad0fac84887d2778416fd791f57970
luna <josueenrique1999@gmail.com>
2018-11-11 12:39:05 -0600

    Identificacion de jugador por jugada

--- app/src/main/java/com/afterbyte/battleship_coldwar/Locate.java
+++ app/src/main/java/com/afterbyte/battleship_coldwar/Locate.java
@@ -3,7 +3,10 @@ package com.afterbyte.battleship_coldwar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class Locate extends AppCompatActivity {
     private String turnString="Your Turn";
@@ -12,14 +15,16 @@ public class Locate extends AppCompatActivity {
     public String getTurnString(){
         return turnString;
     }
-    /*public String setTurnString(){
+    public void setTurnString(){
         if(turnString.equals("Your Turn")){
             turnString="Enemy Turn";
+            player=2;
         }
         else{
             turnString="Your Turn";
+            player=1;
         }
-    }*/
+    }
 
     private int matrix[][]=new int[3][3];
 
@@ -30,9 +35,12 @@ public class Locate extends AppCompatActivity {
     public void setMatrix(int x,int y,int i){
         matrix[x][y]=i;
     }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_locate);
 
 
@@ -49,6 +57,10 @@ public class Locate extends AppCompatActivity {
         }
         Button Btn00=(Button) findViewById(R.id.btn00);
         Btn00.setEnabled(false);
+        //Change turn Message
+        TextView turnMessage=(TextView) findViewById(R.id.turnMessage);
+        setTurnString();
+        turnMessage.setText(getTurnString());
     }
     public void Btn01Action(View v){
         if(player==1){
@@ -61,6 +73,10 @@ public class Locate extends AppCompatActivity {
         }
         Button Btn02=(Button) findViewById(R.id.Btn01);
         Btn02.setEnabled(false);
+        //Change turn Message
+        TextView turnMessage=(TextView) findViewById(R.id.turnMessage);
+        setTurnString();
+        turnMessage.setText(getTurnString());
     }
 
     public void Btn02Action(View v){
@@ -74,6 +90,11 @@ public class Locate extends AppCompatActivity {
         }
         Button Btn02=(Button) findViewById(R.id.Btn02);
         Btn02.setEnabled(false);
+        Btn02.setBackgroundColor(999999);
+        //Change turn Message
+        TextView turnMessage=(TextView) findViewById(R.id.turnMessage);
+        setTurnString();
+        turnMessage.setText(getTurnString());
     }
 
     public void Btn10Action(View v){
@@ -87,6 +108,10 @@ public class Locate extends AppCompatActivity {
         }
         Button Btn10=(Button) findViewById(R.id.Btn10);
         Btn10.setEnabled(false);
+        //Change turn Message
+        TextView turnMessage=(TextView) findViewById(R.id.turnMessage);
+        setTurnString();
+        turnMessage.setText(getTurnString());
     }
 
     public void Btn11Action(View v){
@@ -100,6 +125,10 @@ public class Locate extends AppCompatActivity {
         }
         Button Btn11=(Button) findViewById(R.id.Btn11);
         Btn11.setEnabled(false);
+        //Change turn Message
+        TextView turnMessage=(TextView) findViewById(R.id.turnMessage);
+        setTurnString();
+        turnMessage.setText(getTurnString());
     }
 
     public void Btn12Action(View v){
@@ -113,6 +142,10 @@ public class Locate extends AppCompatActivity {
         }
         Button Btn12=(Button) findViewById(R.id.Btn12);
         Btn12.setEnabled(false);
+        //Change turn Message
+        TextView turnMessage=(TextView) findViewById(R.id.turnMessage);
+        setTurnString();
+        turnMessage.setText(getTurnString());
     }
 
     public void Btn20Action(View v){
@@ -126,6 +159,10 @@ public class Locate extends AppCompatActivity {
         }
         Button Btn20=(Button) findViewById(R.id.Btn20);
         Btn20.setEnabled(false);
+        //Change turn Message
+        TextView turnMessage=(TextView) findViewById(R.id.turnMessage);
+        setTurnString();
+        turnMessage.setText(getTurnString());
     }
 
     public void Btn21Action(View v){
@@ -139,6 +176,10 @@ public class Locate extends AppCompatActivity {
         }
         Button Btn21=(Button) findViewById(R.id.Btn21);
         Btn21.setEnabled(false);
+        //Change turn Message
+        TextView turnMessage=(TextView) findViewById(R.id.turnMessage);
+        setTurnString();
+        turnMessage.setText(getTurnString());
     }
 
     public void Btn22Action(View v){
@@ -152,6 +193,38 @@ public class Locate extends AppCompatActivity {
         }
         Button Btn22=(Button) findViewById(R.id.Btn22);
         Btn22.setEnabled(false);
+        //Change turn Message
+        TextView turnMessage=(TextView) findViewById(R.id.turnMessage);
+        setTurnString();
+        turnMessage.setText(getTurnString());
+    }
+
+    public void isComplete(){
+        int sum=0;
+        //Check file per file
+        for(int x=0;x<3;x++){
+            for(int y=0;y<3;y++){
+                sum+=matrix[x][y];
+            }
+            if(sum==30){
+                TextView turnMessage=(TextView) findViewById(R.id.turnMessage);
+                turnMessage.setText("YOU LOSE");
+                return;
+            }
+            else if(sum==3){
+                TextView turnMessage=(TextView) findViewById(R.id.turnMessage);
+                turnMessage.setText("YOU WIN");
+                return;
+            }
+        }
+        //Check Column per column
+        sum=0;
+        for(int y=0;y<3;y++){
+            for(int x=0;x<3;x++){
+                sum+=matrix[x][y];
+            }
+        }
+
     }
 }
 
