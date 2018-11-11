82840d7198a8b88275fab2ac6c1449bf30691532
luna <josueenrique1999@gmail.com>
2018-11-11 14:36:48 -0600

    Logica casos de juego vs computadora

--- app/src/main/java/com/afterbyte/battleship_coldwar/Locate.java
+++ app/src/main/java/com/afterbyte/battleship_coldwar/Locate.java
@@ -57,10 +57,8 @@ public class Locate extends AppCompatActivity {
         }
         Button Btn00=(Button) findViewById(R.id.btn00);
         Btn00.setEnabled(false);
-        //Change turn Message
-        TextView turnMessage=(TextView) findViewById(R.id.turnMessage);
-        setTurnString();
-        turnMessage.setText(getTurnString());
+        verifyGame();
+
     }
     public void Btn01Action(View v){
         if(player==1){
@@ -73,10 +71,7 @@ public class Locate extends AppCompatActivity {
         }
         Button Btn02=(Button) findViewById(R.id.Btn01);
         Btn02.setEnabled(false);
-        //Change turn Message
-        TextView turnMessage=(TextView) findViewById(R.id.turnMessage);
-        setTurnString();
-        turnMessage.setText(getTurnString());
+        verifyGame();
     }
 
     public void Btn02Action(View v){
@@ -91,10 +86,7 @@ public class Locate extends AppCompatActivity {
         Button Btn02=(Button) findViewById(R.id.Btn02);
         Btn02.setEnabled(false);
         Btn02.setBackgroundColor(999999);
-        //Change turn Message
-        TextView turnMessage=(TextView) findViewById(R.id.turnMessage);
-        setTurnString();
-        turnMessage.setText(getTurnString());
+        verifyGame();
     }
 
     public void Btn10Action(View v){
@@ -108,10 +100,7 @@ public class Locate extends AppCompatActivity {
         }
         Button Btn10=(Button) findViewById(R.id.Btn10);
         Btn10.setEnabled(false);
-        //Change turn Message
-        TextView turnMessage=(TextView) findViewById(R.id.turnMessage);
-        setTurnString();
-        turnMessage.setText(getTurnString());
+        verifyGame();
     }
 
     public void Btn11Action(View v){
@@ -125,10 +114,7 @@ public class Locate extends AppCompatActivity {
         }
         Button Btn11=(Button) findViewById(R.id.Btn11);
         Btn11.setEnabled(false);
-        //Change turn Message
-        TextView turnMessage=(TextView) findViewById(R.id.turnMessage);
-        setTurnString();
-        turnMessage.setText(getTurnString());
+        verifyGame();
     }
 
     public void Btn12Action(View v){
@@ -142,10 +128,7 @@ public class Locate extends AppCompatActivity {
         }
         Button Btn12=(Button) findViewById(R.id.Btn12);
         Btn12.setEnabled(false);
-        //Change turn Message
-        TextView turnMessage=(TextView) findViewById(R.id.turnMessage);
-        setTurnString();
-        turnMessage.setText(getTurnString());
+        verifyGame();
     }
 
     public void Btn20Action(View v){
@@ -159,10 +142,7 @@ public class Locate extends AppCompatActivity {
         }
         Button Btn20=(Button) findViewById(R.id.Btn20);
         Btn20.setEnabled(false);
-        //Change turn Message
-        TextView turnMessage=(TextView) findViewById(R.id.turnMessage);
-        setTurnString();
-        turnMessage.setText(getTurnString());
+        verifyGame();
     }
 
     public void Btn21Action(View v){
@@ -176,10 +156,7 @@ public class Locate extends AppCompatActivity {
         }
         Button Btn21=(Button) findViewById(R.id.Btn21);
         Btn21.setEnabled(false);
-        //Change turn Message
-        TextView turnMessage=(TextView) findViewById(R.id.turnMessage);
-        setTurnString();
-        turnMessage.setText(getTurnString());
+        verifyGame();
     }
 
     public void Btn22Action(View v){
@@ -193,28 +170,44 @@ public class Locate extends AppCompatActivity {
         }
         Button Btn22=(Button) findViewById(R.id.Btn22);
         Btn22.setEnabled(false);
-        //Change turn Message
+        verifyGame();
+    }
+
+    public void changeTurnMessage(){
         TextView turnMessage=(TextView) findViewById(R.id.turnMessage);
         setTurnString();
         turnMessage.setText(getTurnString());
     }
 
-    public void isComplete(){
+    public boolean isComplete(){
+        //RETURNS TRUE IF THERE ARE NO CELLS AVAILABLE, FALSE IF STILL THERE ARE CELL AVAILABLE
+        int cont=0;
+        for(int x=0;x<3;x++) {
+            for (int y = 0; y < 3; y++) {
+                if (matrix[x][y] == 0) {
+                    cont++;
+                }
+            }
+        }
+        return cont==0;
+    }
+
+    public int winCheck(){
+        //RETURN 0 IF NOBODY WINS, 1 IF PLAER 1 WINS, 2 IF PLAYER 2 WINS
         int sum=0;
         //Check file per file
         for(int x=0;x<3;x++){
             for(int y=0;y<3;y++){
                 sum+=matrix[x][y];
             }
-            if(sum==30){
-                TextView turnMessage=(TextView) findViewById(R.id.turnMessage);
-                turnMessage.setText("YOU LOSE");
-                return;
+            if(sum==3){
+                return 1;
             }
-            else if(sum==3){
-                TextView turnMessage=(TextView) findViewById(R.id.turnMessage);
-                turnMessage.setText("YOU WIN");
-                return;
+            else if(sum==30) {
+                return 2;
+            }
+            else{
+                sum=0;
             }
         }
         //Check Column per column
@@ -223,10 +216,106 @@ public class Locate extends AppCompatActivity {
             for(int x=0;x<3;x++){
                 sum+=matrix[x][y];
             }
+            if(sum==3){
+                return 1;
+            }
+            else if(sum==30) {
+                return 2;
+            }
+            else{
+                sum=0;
+            }
         }
+        //Check Main Diagonal
+        sum=0;
+        for(int i=0;i<3;i++){
+            sum+=matrix[i][i];
+        }
+        if(sum==3){
+            return 1;
+        }
+        else if(sum==30) {
+            return 2;
+        }
+        //Check inverted main diagonal
+        sum=0;
+        sum+=matrix[0][2];
+        sum+=matrix[1][1];
+        sum+=matrix[2][0];
+        if(sum==3){
+            return 1;
+        }
+        else if(sum==30) {
+            return 2;
+        }
+        else {
+            return 0;
+        }
+    }
 
+    public void setWinnerMessage(int player){
+        if(player==2){
+            TextView turnMessage=(TextView) findViewById(R.id.turnMessage);
+            turnMessage.setText("YOU LOSE");
+        }
+        else if(player==1){
+            TextView turnMessage=(TextView) findViewById(R.id.turnMessage);
+            turnMessage.setText("YOU WIN");
+        }
+    }
+
+    public void verifyGame(){
+
+        //CHECK IF TABLE IS FULL
+        if(isComplete()){
+            int winner=winCheck();
+            if(winner==0){
+                TextView turnMessage=(TextView) findViewById(R.id.turnMessage);
+                turnMessage.setText("TIED GAME");
+            }
+            else{
+                setWinnerMessage(winner);
+                setAllButtonsEnabled(false);
+            }
+        }
+        else{
+            int winner=winCheck();
+            if(winner==0){
+                changeTurnMessage();
+            }
+            else{
+                setWinnerMessage(winner);
+                setAllButtonsEnabled(false);
+            }
+        }
+    }
+
+    public void setAllButtonsEnabled(boolean b){
+        Button Btn00=(Button) findViewById(R.id.btn00);
+        Btn00.setEnabled(b);
+
+        Button Btn01=(Button) findViewById(R.id.Btn01);
+        Btn01.setEnabled(b);
+
+        Button Btn02=(Button) findViewById(R.id.Btn02);
+        Btn02.setEnabled(b);
+
+        Button Btn10=(Button) findViewById(R.id.Btn10);
+        Btn10.setEnabled(b);
+
+        Button Btn11=(Button) findViewById(R.id.Btn11);
+        Btn11.setEnabled(b);
+
+        Button Btn12=(Button) findViewById(R.id.Btn12);
+        Btn12.setEnabled(b);
+
+        Button Btn20=(Button) findViewById(R.id.Btn20);
+        Btn20.setEnabled(b);
+
+        Button Btn21=(Button) findViewById(R.id.Btn21);
+        Btn21.setEnabled(b);
+
+        Button Btn22=(Button) findViewById(R.id.Btn22);
+        Btn22.setEnabled(b);
     }
 }
-
-
-
